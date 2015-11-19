8ec946c000b1b4aa4c60eafb7a7059c28e7414e2
Matthis Perrin <matthis.perrin@gmail.com>
2015-11-18 17:33:22 -0800

    Add initializer for BitcoinCore classes

--- MyBits/BitcoinCore.swift
+++ MyBits/BitcoinCore.swift
@@ -1,16 +1,71 @@
 import Foundation
 
-class BitcoinAddress     { var value: String = ""       }
-class BlockHash          { var value: String = ""       }
-class BlockHeight        { var value: Int = 0           }
-class BitcoinScript      { var value: String = ""       }
-class TxConfidence       { var value: Int = 0           }
-class TxConfirmations    { var value: Int = 0           }
-class TxConfirmationTime { var value: NSDate = NSDate() }
-class TxReceptionTime    { var value: NSDate = NSDate() }
-class TxHash             { var value: String = ""       }
-class TxSize             { var value: Int = 0           }
-class TxLockTime         { var value: int = 0           }
+class BitcoinAddress {
+    var value: String = ""
+    init(value: String = "") {
+        self.value = value
+    }
+}
+class BlockHash {
+    var value: String = ""
+    init(value: String = "") {
+        self.value = value
+    }
+}
+class BlockHeight {
+    var value: Int = 0
+    init(value: Int = 0) {
+        self.value = value
+    }
+}
+class BitcoinScript {
+    var value: String = ""
+    init(value: String = "") {
+        self.value = value
+    }
+}
+class TxConfidence {
+    var value: Int = 0
+    init(value: Int = 0) {
+        self.value = value
+    }
+}
+class TxConfirmations {
+    var value: Int = 0
+    init(value: Int = 0) {
+        self.value = value
+    }
+}
+class TxConfirmationTime {
+    var value: NSDate = NSDate()
+    init(value: NSDate = NSDate()) {
+        self.value = value
+    }
+}
+class TxReceptionTime {
+    var value: NSDate = NSDate()
+    init(value: NSDate = NSDate()) {
+        self.value = value
+    }
+}
+class TxHash {
+    var value: String = ""
+    init(value: String = "") {
+        self.value = value
+    }
+}
+class TxSize {
+    var value: Int = 0
+    init(value: Int = 0) {
+        self.value = value
+    }
+}
+class TxLockTime {
+    var value: Int = 0
+    init(value: Int = 0) {
+        self.value = value
+    }
+}
 class TxFee: BitcoinAmount {}
 
 enum BitcoinScriptType {
