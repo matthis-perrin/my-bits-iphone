e941085c886aeaefd5288fb3c86f1fc9ac7f3daf
Matthis Perrin <matthis.perrin@gmail.com>
2015-11-29 06:32:21 -0800

    Fix warnings with TransactionTableViewCell constraints

--- MyBits/TransactionTableViewCell.swift
+++ MyBits/TransactionTableViewCell.swift
@@ -11,26 +11,29 @@ class TransactionTableViewCell: UITableViewCell {
         self.contentView.addSubview(self.transactionViewController.view)
 
         self.transactionViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint(
+        let topConstraint = NSLayoutConstraint(
             item: self.transactionViewController.view, attribute: .Top,
             relatedBy: .Equal,
             toItem: self.contentView, attribute: .Top,
-            multiplier: 1.0, constant: 0).active = true
-        NSLayoutConstraint(
+            multiplier: 1.0, constant: 0)
+        let rightConstraint = NSLayoutConstraint(
             item: self.transactionViewController.view, attribute: .Right,
             relatedBy: .Equal,
             toItem: self.contentView, attribute: .Right,
-            multiplier: 1.0, constant: 0).active = true
-        NSLayoutConstraint(
+            multiplier: 1.0, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(
             item: self.transactionViewController.view, attribute: .Bottom,
             relatedBy: .Equal,
             toItem: self.contentView, attribute: .Bottom,
-            multiplier: 1.0, constant: 0).active = true
-        NSLayoutConstraint(
+            multiplier: 1.0, constant: 0)
+        let leftConstraint = NSLayoutConstraint(
             item: self.transactionViewController.view, attribute: .Left,
             relatedBy: .Equal,
             toItem: self.contentView, attribute: .Left,
-            multiplier: 1.0, constant: 0).active = true
+            multiplier: 1.0, constant: 0)
+
+        bottomConstraint.priority = UILayoutPriorityFittingSizeLevel
+        NSLayoutConstraint.activateConstraints([topConstraint, rightConstraint, bottomConstraint, leftConstraint])
     }
 
     required convenience init(coder: NSCoder) {
