86b3bd90f1ece6ea30866054b8be227b94a2f9c9
Matthis Perrin <matthis.perrin@gmail.com>
2015-11-29 06:56:34 -0800

    Allow to filter transactions by accounts in the TransactionTableViewController

--- MyBits/TransactionTableViewController.swift
+++ MyBits/TransactionTableViewController.swift
@@ -2,6 +2,20 @@ import UIKit
 
 class TransactionTableViewController: UITableViewController, AllTransactionsProtocol {
 
+    // Empty array = All accounts (no filtering)
+    var accounts: [Account]
+    private var txs: [BitcoinTx]
+
+    init(accounts: [Account] = [Account]()) {
+        self.accounts = accounts
+        self.txs = [BitcoinTx]()
+        super.init(style: .Plain)
+    }
+
+    required convenience init?(coder: NSCoder) {
+        self.init(accounts: [])
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = NSLocalizedString("transactions", comment: "")
@@ -16,29 +30,38 @@ class TransactionTableViewController: UITableViewController, AllTransactionsProt
 
     func transactionReceived(tx: BitcoinTx) {
         dispatch_async(dispatch_get_main_queue(), {
+            self.txs = TransactionStore.getTransactions()
+                .filter() { tx in
+                    return self.accounts.isEmpty ||
+                        tx.getInfo().involvedAccounts.contains() { txAccount in
+                            return self.accounts.contains() { filteredAccount in
+                                return txAccount.getId() == filteredAccount.getId()
+                            }
+                        }
+                }
+                .sort() { tx1, tx2 in
+                    if tx1.blockHeight.value == tx2.blockHeight.value {
+                        return tx1.receptionTime.value.compare(tx2.receptionTime.value) == NSComparisonResult.OrderedAscending
+                    } else {
+                        if tx1.blockHeight.value < 0 {
+                            return true
+                        } else if tx2.blockHeight.value < 0 {
+                            return false
+                        } else {
+                            return tx1.receptionTime.value.compare(tx2.receptionTime.value) == NSComparisonResult.OrderedDescending
+                        }
+                    }
+                }
             self.tableView.reloadData()
         })
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TransactionStore.getTransactions().count
+        return self.txs.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let sortedTxs = TransactionStore.getTransactions().sort() { tx1, tx2 in
-            if tx1.blockHeight.value == tx2.blockHeight.value {
-                return tx1.receptionTime.value.compare(tx2.receptionTime.value) == NSComparisonResult.OrderedAscending
-            } else {
-                if tx1.blockHeight.value < 0 {
-                    return false
-                } else if tx2.blockHeight.value < 0 {
-                    return true
-                } else {
-                    return tx1.receptionTime.value.compare(tx2.receptionTime.value) == NSComparisonResult.OrderedDescending
-                }
-            }
-        }
-        let tx = sortedTxs[indexPath.row]
+        let tx = self.txs[indexPath.row]
         if let cell = self.tableView.dequeueReusableCellWithIdentifier(TransactionTableViewCell.reusableIdentifierFor(tx)) as? TransactionTableViewCell {
             cell.setTx(tx)
             return cell
