f8a5597cf1efcdf7d6c09c0cee4b986bd9175a9a
Benjamin Meudre <bmeudre@gmail.com>
2015-12-05 21:02:26 +0100

    Added foreign keys

--- MyBits/Database.swift
+++ MyBits/Database.swift
@@ -6,6 +6,7 @@ class DB {
     private static let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
     private static var conn: Connection!
     static var useTestDB = false
+    static var debug = true
 
     // Accounts table
     private static let accounts = Table("accounts")
@@ -26,22 +27,26 @@ class DB {
     private static let bitcoinAddressMasterPublicKeyId = Expression<Int64?>("master_public_key_id")
 
     static func empty() {
+        let dbPath = DB.getDBPath()
         do {
-            try NSFileManager().removeItemAtPath(DB.getDBPath())
+            try NSFileManager().removeItemAtPath(dbPath)
         } catch {}
+        log("Successful deletion of file \(dbPath)")
     }
 
-    static func getDBPath() -> String {
+    private static func getDBPath() -> String {
         let suffix = DB.useTestDB ? "-test" : ""
         return "\(DB.path)/db\(suffix).sqlite3"
     }
 
     static func initialize() {
         // Creating database connection
-        self.conn = try! Connection(DB.getDBPath())
+        let dbPath = DB.getDBPath()
+        self.conn = try! Connection(dbPath)
         guard let db = self.conn else {
             return
         }
+        log("Successful connection using file \(dbPath)")
 
         // SQL queries logs
 //        db.trace { x in print(x + "\n") }
@@ -57,15 +62,18 @@ class DB {
             try db.run(self.masterPublicKeys.create(ifNotExists: true) { t in
                 t.column(self.masterPublicKeyId, primaryKey: true)
                 t.column(self.masterPublicKeyValue)
-                t.column(self.masterPublicKeyAccountId, references: self.accounts, self.accountId)
+                t.column(self.masterPublicKeyAccountId)
+                t.foreignKey(self.masterPublicKeyAccountId, references: self.accounts, self.accountId, delete: .Cascade)
                 })
 
             // Creating bitcoin addresses table
             try db.run(self.bitcoinAddresses.create(ifNotExists: true) { t in
                 t.column(self.bitcoinAddressId, primaryKey: true)
                 t.column(self.bitcoinAddressValue)
-                t.column(self.self.bitcoinAddressAccountId, references: self.accounts, self.accountId)
-                t.column(self.self.bitcoinAddressMasterPublicKeyId, references: self.masterPublicKeys, self.masterPublicKeyId)
+                t.column(self.bitcoinAddressAccountId)
+                t.column(self.bitcoinAddressMasterPublicKeyId)
+                t.foreignKey(self.bitcoinAddressAccountId, references: self.accounts, self.accountId, delete: .Cascade)
+                t.foreignKey(self.bitcoinAddressMasterPublicKeyId, references: self.masterPublicKeys, self.masterPublicKeyId, delete: .Cascade)
                 })
             
             // Initialize stores
@@ -83,6 +91,7 @@ class DB {
         for row in db.prepare(self.accounts) {
             res.append(Account(accountId: AccountId(value: row[self.accountId]), accountName: row[self.accountName]))
         }
+        log("Found \(res.count) accounts")
         return res
     }
 
@@ -99,6 +108,7 @@ class DB {
         for row in rows {
             res.append(MasterPublicKey(masterPublicKeyId: MasterPublicKeyId(value: row[self.masterPublicKeyId]), value: row[self.masterPublicKeyValue]))
         }
+        log("Found \(res.count) master public keys for account \(account.getName())")
         return res
     }
 
@@ -115,6 +125,7 @@ class DB {
         for row in rows {
             res.append(BitcoinAddress(bitcoinAddressId: BitcoinAddressId(value: row[self.bitcoinAddressId]), value: row[self.bitcoinAddressValue]))
         }
+        log("Found \(res.count) bitcoin addresses for account \(account.getName())")
         return res
     }
 
@@ -131,6 +142,7 @@ class DB {
         for row in rows {
             res.append(BitcoinAddress(bitcoinAddressId: BitcoinAddressId(value: row[self.bitcoinAddressId]), value: row[self.bitcoinAddressValue]))
         }
+        log("Found \(res.count) bitcoin addresses for master public key \(masterPublicKey.value)")
         return res
     }
 
@@ -142,6 +154,7 @@ class DB {
             self.accountId <- account.getId().value,
             self.accountName <- account.getName()
             ))
+        log("Inserted account \(account.getName())")
     }
 
     static func insertMasterPublicKey(masterPublicKey: MasterPublicKey, account: Account) throws {
@@ -153,6 +166,7 @@ class DB {
             self.masterPublicKeyValue <- masterPublicKey.value,
             self.masterPublicKeyAccountId <- account.getId().value
             ))
+        log("Inserted master public key \(masterPublicKey.value) for account \(account.getName())")
     }
 
     static func insertBitcoinAddress(bitcoinAddress: BitcoinAddress, account: Account) throws {
@@ -164,6 +178,7 @@ class DB {
             self.bitcoinAddressValue <- bitcoinAddress.value,
             self.bitcoinAddressAccountId <- account.getId().value
             ))
+        log("Inserted bitcoin address \(bitcoinAddress.value) for account \(account.getName())")
     }
 
     static func insertBitcoinAddress(bitcoinAddress: BitcoinAddress, masterPublicKey: MasterPublicKey) throws {
@@ -175,6 +190,13 @@ class DB {
             self.bitcoinAddressValue <- bitcoinAddress.value,
             self.bitcoinAddressMasterPublicKeyId <- masterPublicKey.masterPublicKeyId.value
             ))
+        log("Inserted bitcoin address \(bitcoinAddress.value) for master public key \(masterPublicKey.value)")
+    }
+
+    private static func log(message: String) {
+        if debug {
+            NSLog("[Database] \(message)")
+        }
     }
 
 }
